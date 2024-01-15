6fce13273a03157104a70ef8aeb53d49e9e27080
ReDMallard <marco@titori.it>
2024-01-15 01:38:10 +0100

    same with echo and method()

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ require __DIR__ . "/Models/Movie.php";
 
 $Il_Signore_degli_anelli = new Movie("Il Signore degli Anelli","2001","epico", "New Zeland", "2h42min");
 
-var_dump($Il_Signore_degli_anelli);
+echo $Il_Signore_degli_anelli->getMovieData();
